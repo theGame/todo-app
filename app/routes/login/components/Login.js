963e68d7f0f1293350cb780151d399e1ce587061
yuriy.voytas <yuriy.voytas@knowledgevision.com>
2017-12-08 14:57:19 +0200

    updated react, prop-types

--- app/routes/login/components/Login.js
+++ app/routes/login/components/Login.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Col, Form, FormGroup, FormControl, Checkbox, Button, ControlLabel } from 'react-bootstrap';
 
 const Login = ({  }) => {
